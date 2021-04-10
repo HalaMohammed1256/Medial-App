eac59e16e57c4127d2c52120a53cfd33b57534a8
Hala Mohammed <HalaMohammed1245@gmail.com>
2021-04-10 12:58:21 +0200

    Initial Commit

---
+++ MedicalApplication/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MedicalApplication
+//
+//  Created by Hala on 10/04/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
